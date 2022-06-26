b618326b87ab40b6c60960164f04cbba089e514a
novokhatsky <program_maker@mail.ru>
2022-06-26 16:57:40 +0700

    запись в новый файл, добавлено логгирование

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 from openpyxl import load_workbook
 import os
+import logging
 
 BASE_DIR = "d:\\tmp\\rubcov\\большие\\"
+PREFIX_OUT = "out_"
 
 PPR = BASE_DIR + "пэн-ппр\\ППР.xlsx"
 PEN = BASE_DIR + "пэн-ппр\\ПЭН.xlsx"
@@ -11,14 +13,20 @@ PEN = BASE_DIR + "пэн-ппр\\ПЭН.xlsx"
 PPR_INDEX = [38, 39, 40, 41, 50, 51, 52, 53, 54, 55, 91, 92, 93, 94, 95, 96, 97, 98]
 PEN_INDEX = [35, 36, 37, 42, 43, 44, 45, 46, 47, 48, 49, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
 
+LOG_FILE = "process.log"
 
-def dictForSave(i):
-    temp = {}
-    for index in FROM_PPR:
-        temp[index] = ppr_sheet['{0}{1}'.format(index, i)].value
+logging.basicConfig(format = u'[%(asctime)s] %(message)s', level = logging.INFO, filename = LOG_FILE)
 
-    return temp
 
+def out_log(mess):
+    logging.info(mess)
+
+
+def makeOut(filename):
+    el = filename.split("\\")
+    el[-1] = PREFIX_OUT + el[-1]
+
+    return "\\".join(el)
 
 def makeDict(filename):
     wb = load_workbook(filename, read_only = True)
@@ -51,9 +59,12 @@ def filesKurators(base_dir):
 
 
 print('load ppr')
+out_log("загрузка ППР")
+
 ppr_dict = makeDict(PPR)
 
 print('load pen')
+out_log("загрузка ПЭН")
 pen_dict = makeDict(PEN)
 
 # получение списка файлов кураторов
@@ -64,6 +75,7 @@ for fl in files_kurators:
     #fl = files_kurators[1]
 
     print('processing {0}'.format(fl))
+    out_log("обработка {0}".format(fl))
 
     wb = load_workbook(fl)
     sh = wb.active
@@ -82,15 +94,26 @@ for fl in files_kurators:
 
             if key in ppr_dict:
                 # есть идентификатор в ппр
+                out_log("найден идентификатор {0} в ППР".format(key))
+
                 for index in PPR_INDEX:
-                    row[index-1].value = ppr_dict[key][index - 1]
+                    out_log("в поле {0} вставлено значение {1}".format(index, ppr_dict[key][index - 1]))
+
+                    row[index - 1].value = ppr_dict[key][index - 1]
 
             if key in pen_dict:
                 # есть идентификатор в ппр
-                for index in PEN_INDEX:
-                    row[index-1].value = pen_dict[key][index - 1]
+                out_log("найден идентификатор {0} в ПЭН".format(key))
 
-    wb.save(fl)
+                for index in PEN_INDEX:
+                    out_log("в поле {0} вставлено значение {1}".format(index, pen_dict[key][index - 1]))
+
+                    row[index - 1].value = pen_dict[key][index - 1]
+
+    print("save {0}".format(makeOut(fl)))
+    out_log("запись {0}".format(makeOut(fl)))
+    wb.save(makeOut(fl))
+
 # ппр 38-41 50-55 91-98 AL-AM-AN-AO AX-AY-AZ-BA-BB-DC CM-CN-CO-CP-CQ-CR-CS-CT
 # пэн 35-37 42-49 74-90
 
