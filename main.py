b9bf072047976e7eaeead2b34d48c97a8983bceb
novokhatsky <program_maker@mail.ru>
2022-06-26 09:30:18 +0700

    получили список файлов кураторов

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openpyxl import load_workbook
+import os
 
 BASE_DIR = "d:\\tmp\\rubcov\\большие\\"
 
@@ -24,12 +25,27 @@ def makeDict(filename):
         data[row[0].value] = [cell.value for cell in row]
 
     return data
+def filesKurators(base_dir):
+    # формируем список файлов с заданной маской в директории
+    spisok_file = []
+    for i in os.listdir(base_dir):
+        if os.path.isfile(os.path.join(base_dir, i)):
+           if i.endswith('.xlsx'):
+                spisok_file.append(i)
+
+    return spisok_file
 
 print('load ppr')
-ppr_dict = makeDict(PPR)
+#ppr_dict = makeDict(PPR)
 
 print('load pen')
-pen_dict = makeDict(PEN)
+#pen_dict = makeDict(PEN)
+
+# получение списка файлов кураторов
+files_kurators = filesKurators(BASE_DIR)
+
+for fl in files_kurators:
+    print(fl)
 
 # ппр 38-41 50-55 91-98 AL-AM-AN-AO AX-AY-AZ-BA-BB-DC CM-CN-CO-CP-CQ-CR-CS-CT
 # пэн 35-37 42-49 74-90
