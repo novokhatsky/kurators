09568a1f1769a4e5594e02b6136d0c79f8fddc75
novokhatsky <program_maker@mail.ru>
2022-06-26 13:53:16 +0700

    обработка файла куратора

--- main.py
+++ main.py
@@ -16,27 +16,39 @@ def dictForSave(i):
 
     return temp
 
+
 def makeDict(filename):
     wb = load_workbook(filename, read_only = True)
     sh = wb.active
     data = {} 
 
+    # нужно пропустить три строки
+    enable_add = False
     for row in sh.iter_rows():
-        data[row[0].value] = [cell.value for cell in row]
+
+        if row[0].value == 'Идентификатор':
+            enable_add = True
+            continue
+    
+        if enable_add:
+            data[row[0].value] = [cell.value for cell in row]
 
     return data
+
+
 def filesKurators(base_dir):
     # формируем список файлов с заданной маской в директории
     spisok_file = []
     for i in os.listdir(base_dir):
         if os.path.isfile(os.path.join(base_dir, i)):
            if i.endswith('.xlsx'):
-                spisok_file.append(i)
+                spisok_file.append(os.path.join(base_dir, i))
 
     return spisok_file
 
+
 print('load ppr')
-#ppr_dict = makeDict(PPR)
+ppr_dict = makeDict(PPR)
 
 print('load pen')
 #pen_dict = makeDict(PEN)
@@ -44,8 +56,18 @@ print('load pen')
 # получение списка файлов кураторов
 files_kurators = filesKurators(BASE_DIR)
 
-for fl in files_kurators:
-    print(fl)
+#for fl in files_kurators:
+
+fl = files_kurators[1]
+
+print(fl)
+kurator = makeDict(fl)
+
+for key in kurator:
+    if key in ppr_dict:
+        print('found ppr key={0} 1={1}'.format(key, ppr_dict[key][0]))
+
+
 
 # ппр 38-41 50-55 91-98 AL-AM-AN-AO AX-AY-AZ-BA-BB-DC CM-CN-CO-CP-CQ-CR-CS-CT
 # пэн 35-37 42-49 74-90
