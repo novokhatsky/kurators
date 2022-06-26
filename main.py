88a211bc81fd2f7f4c3a9496352fceafa568abc4
novokhatsky <program_maker@mail.ru>
2022-06-26 14:50:46 +0700

    черновой рабочий вариант

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ PPR = BASE_DIR + "пэн-ппр\\ППР.xlsx"
 PEN = BASE_DIR + "пэн-ппр\\ПЭН.xlsx"
 
 PPR_INDEX = [38, 39, 40, 41, 50, 51, 52, 53, 54, 55, 91, 92, 93, 94, 95, 96, 97, 98]
+PEN_INDEX = [35, 36, 37, 42, 43, 44, 45, 46, 47, 48, 49, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+
 
 def dictForSave(i):
     temp = {}
@@ -52,42 +54,43 @@ print('load ppr')
 ppr_dict = makeDict(PPR)
 
 print('load pen')
-#pen_dict = makeDict(PEN)
+pen_dict = makeDict(PEN)
 
 # получение списка файлов кураторов
 files_kurators = filesKurators(BASE_DIR)
 
-#for fl in files_kurators:
+for fl in files_kurators:
 
-fl = files_kurators[1]
+    #fl = files_kurators[1]
 
-print(fl)
+    print('processing {0}'.format(fl))
 
-wb = load_workbook(fl, read_only = True)
-sh = wb.active
-data = {} 
+    wb = load_workbook(fl)
+    sh = wb.active
+    data = {} 
 
-# нужно пропустить три строки
-enable_add = False
-for row in sh.iter_rows():
+    # нужно пропустить три строки
+    enable_add = False
+    for row in sh.iter_rows():
 
-    if row[0].value == 'Идентификатор':
-        enable_add = True
-        continue
+        if row[0].value == 'Идентификатор':
+            enable_add = True
+            continue
 
-    if enable_add:
-        key = row[0].value
+        if enable_add:
+            key = row[0].value
 
-        if key in ppr_dict:
-            print(key)
-            # есть идентификатор в ппр
-            for index in PPR_INDEX:
-                print('{0} = {1}'.format(index + 1, ppr_dict[index + 1]))
+            if key in ppr_dict:
+                # есть идентификатор в ппр
+                for index in PPR_INDEX:
+                    row[index-1].value = ppr_dict[key][index - 1]
 
+            if key in pen_dict:
+                # есть идентификатор в ппр
+                for index in PEN_INDEX:
+                    row[index-1].value = pen_dict[key][index - 1]
 
-#       data[row[0].value] = [cell.value for cell in row]
-
-#wb.save(fl)
+    wb.save(fl)
 # ппр 38-41 50-55 91-98 AL-AM-AN-AO AX-AY-AZ-BA-BB-DC CM-CN-CO-CP-CQ-CR-CS-CT
 # пэн 35-37 42-49 74-90
 
