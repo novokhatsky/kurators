ebc39d774dc5fcf3f487234b54a1777e164fedfd
novokhatsky <program_maker@mail.ru>
2022-06-26 09:21:42 +0700

    отказ от csv и sqlite в пользу генератора для обработки файла

--- main.py
+++ main.py
@@ -1,36 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from openpyxl import load_workbook
-import sqlite3
-import json
-import csv
-import codecs
 
 BASE_DIR = "d:\\tmp\\rubcov\\большие\\"
 
 PPR = BASE_DIR + "пэн-ппр\\ППР.xlsx"
-
-FROM_PPR = ['AL', 'AM', 'AN', 'AO', 'AX', 'AY', 'AZ', 'BA', 'BB', 'DC', 'CM', 'CN', 'CO', 'CP', 'CQ', 'CR', 'CS', 'CT']
-
-db = sqlite3.connect('xml.db')
-
-cursor = db.cursor()
-
-cursor.execute('''
-    select count(name) from sqlite_master where type = 'table' and name = 'ppr'
-''')
-
-if cursor.fetchone()[0] == 1:
-    # table exists
-    pass
-else:
-    # table not exists
-    cursor.execute('''
-        create table ppr (
-            id varchar (100) primary key,
-            data text)
-    ''')
-
+PEN = BASE_DIR + "пэн-ппр\\ПЭН.xlsx"
 
 
 def dictForSave(i):
@@ -40,51 +15,22 @@ def dictForSave(i):
 
     return temp
 
-ppr_wb = load_workbook(PPR, read_only = True)
+def makeDict(filename):
+    wb = load_workbook(filename, read_only = True)
+    sh = wb.active
+    data = {} 
 
-sh = ppr_wb.active
-d = {} 
-for row in sh.iter_rows():
-    print(row[0].value)
-    d[row[0].value] = [cell.value for cell in row]
+    for row in sh.iter_rows():
+        data[row[0].value] = [cell.value for cell in row]
 
-exit()
+    return data
 
-with codecs.open('test.csv', 'w', 'utf-16') as file_handle:
-    csv_writer = csv.writer(file_handle, dialect='excel', delimiter=';')
-    for row in sh.iter_rows(): # generator; was sh.rows
-        csv_writer.writerow([cell.value for cell in row])
+print('load ppr')
+ppr_dict = makeDict(PPR)
 
-exit()
+print('load pen')
+pen_dict = makeDict(PEN)
 
-ppr_sheetnames = ppr_wb.sheetnames
-
-ppr_sheet = ppr_wb[ppr_sheetnames[0]]
-
-ppr_dict = {}
-i = 3
-
-while (True):
-    try:
-        i += 1
-        
-        address = "A{0}".format(i)
-        
-        print('{0} - {1}'.format(i, ppr_sheet[address].value))
-
-        #cursor.execute('''
-        #    insert into ppr (id, data) values (?, ?)
-        #    ''',
-        #    [ppr_sheet[address].value, json.dumps(dictForSave(i))]
-        #)
-
-    except ValueError:
-        print("exception")
-        break;
-print(i)
 # ппр 38-41 50-55 91-98 AL-AM-AN-AO AX-AY-AZ-BA-BB-DC CM-CN-CO-CP-CQ-CR-CS-CT
 # пэн 35-37 42-49 74-90
 
-db.commit()
-
-db.close()
