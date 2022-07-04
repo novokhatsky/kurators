1a594d8d95f88938655bccc938780ef7ff41a025
novokhatsky <program_maker@mail.ru>
2022-07-04 16:17:14 +0700

    исключили формирование общего файла кураторов, изменили процедуру записи

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ import os
 import logging
 from copy import copy
 
+import traceback
+
 #BASE_DIR = "d:\\work\\in\\"
 #BASE_OUT = "d:\\work\\out\\"
 BASE_DIR = "d:\\tmp\\rubcov\\pro\\in\\"
 BASE_OUT = "d:\\tmp\\rubcov\\pro\\out\\"
 
+ALL = BASE_OUT + 'out_all.xlsx'
+
 PPR = BASE_DIR + "пэн-ппр\\ППР.xlsx"
 PEN = BASE_DIR + "пэн-ппр\\ПЭН.xlsx"
 
-PPR_INDEX = [38, 39, 40, 41, 50, 51, 52, 53, 54, 55, 91, 92, 93, 94, 95, 96, 97, 98]
-PEN_INDEX = [35, 36, 37, 42, 43, 44, 45, 46, 47, 48, 49, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+PPR_OUT = BASE_OUT + "пэн-ппр\\ППР.xlsx"
+PEN_OUT = BASE_OUT + "пэн-ппр\\ПЭН.xlsx"
+
+PPR_INDEX_I = [38, 39, 40, 41, 50, 51, 52, 53, 54, 55, 91, 92, 93, 94, 95, 96, 97, 98]
+PEN_INDEX_I = [35, 36, 37, 42, 43, 44, 45, 46, 47, 48, 49, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
+
+PPR_INDEX_II = [20, 21, 74, 75, 76, 77, 78, 79, 80, 81, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98]
+PEN_INDEX_II = [20, 21, 74, 75, 76, 77, 78, 79, 80, 81, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98]
 
 LOG_FILE = BASE_DIR + "process.log"
 
@@ -39,13 +49,15 @@ def makeDict(filename):
     # нужно пропустить три строки
     enable_add = False
     for row in sh.iter_rows():
-
-        if row[0].value == 'Идентификатор':
-            enable_add = True
-            continue
     
         if enable_add:
             data[row[0].value] = [cell.value for cell in row]
+            continue
+
+        if row[0].value == 'Идентификатор':
+            enable_add = True
+
+    wb.close()
 
     return data
 
@@ -59,6 +71,38 @@ def listFiles(base_dir):
                 spisok_file.append(os.path.join(base_dir, i))
 
     return spisok_file
+
+
+def saveAll():
+    filesInput = listFiles(BASE_OUT)
+
+    theOne = Workbook(write_only = True)
+    o = theOne.create_sheet('ПЭН_ППР сокращ')
+    newSheet = theOne[o.title]
+
+    # для первого файла считаем все строки
+    enable_add = True
+    for oneFile in filesInput:
+        print("load {0}".format(oneFile))
+        wb = load_workbook(oneFile, read_only = True)
+        sourceSheet = wb.active
+
+        for row in sourceSheet.iter_rows():
+        
+            if enable_add:
+                newSheet.append([cell.value for cell in row])
+                continue
+
+            if row[0].value == 'Идентификатор':
+                enable_add = True
+
+        # у второго и последующих файлов, пропускаем строки включительно до Иденификатора
+        enable_add = False
+        wb.close()
+
+    print('save all')
+    theOne.save(ALL)
+    theOne.close()
 
 
 print('load ppr')
@@ -88,21 +132,17 @@ for fl in files_kurators:
     data = {} 
 
     # нужно пропустить три строки
-    enable_add = False
+    enable_work = False
     for row in sh.iter_rows():
 
-        if row[0].value == 'Идентификатор':
-            enable_add = True
-            continue
-
-        if enable_add:
+        if enable_work:
             key = row[0].value
 
             if key in ppr_dict:
                 # есть идентификатор в ппр
                 out_log("найден идентификатор {0} в ППР".format(key))
 
-                for index in PPR_INDEX:
+                for index in PPR_INDEX_I:
 
                     if row[index - 1].value != ppr_dict[key][index - 1]:
                         out_log("в поле {0} замена {1} на {2}".format(index, row[index - 1].value, ppr_dict[key][index - 1]))
@@ -114,7 +154,7 @@ for fl in files_kurators:
                 # есть идентификатор в ппр
                 out_log("найден идентификатор {0} в ПЭН".format(key))
 
-                for index in PEN_INDEX:
+                for index in PEN_INDEX_I:
 
                     if row[index - 1].value != pen_dict[key][index - 1]:
                         out_log("в поле {0} замена {1} на {2}".format(index, row[index - 1].value, pen_dict[key][index - 1]))
@@ -122,40 +162,86 @@ for fl in files_kurators:
             else:
                 out_log("идентификатор {0} не найден в ПЭН".format(key))
 
+            continue
+
+        if row[0].value == 'Идентификатор':
+            enable_work = True
+
     out_filename = makeOut(fl)
     print("save {0}".format(out_filename))
     out_log("запись {0}".format(out_filename))
     wb.save(out_filename)
+    wb.close()
+    wb = None
+    pen_dict.clear()
+    ppr_dict.clear()
 
-filesInput = listFiles(BASE_OUT)
-theOneFile = BASE_OUT + 'out_all.xlsx'
+# запись общего файла
+#saveAll()
+
+#второй этап
+#print('load all')
+#out_log("загрузка all")
+
+#all_dict = makeDict(ALL)
+
+print('load kurators')
+dicts = []
+for fl in listFiles(BASE_OUT):
+    dicts.append(makeDict(fl))
+
+print('load ppr')
+wb = load_workbook(PPR)
+sh = wb.active
+
+print('seek ppr')
+
+for row in sh.iter_rows():
+    key = row[0].value
+
+    for dic in dicts:
+
+        if key in dic.keys():
+            
+            for index in PPR_INDEX_II:
+                row[index - 1].value = dic[key][index - 1]
+
+print('save ppr')
+wb.save(PPR_OUT)
+wb.close()
+wb = None
+
+print('load pen')
+wb = load_workbook(PEN, read_only = True)
+sh = wb.active
 
 theOne = Workbook(write_only = True)
 o = theOne.create_sheet('ПЭН_ППР сокращ')
-safeTitle = o.title
+newSheet = theOne[o.title]
 
-newSheet = theOne[safeTitle]
-row_index = 1
-keep3row = True
+print('seek pen')
 
-for oneFile in filesInput:
-    print("load {0}".format(oneFile))
-    wb = load_workbook(oneFile)
-    sourceSheet = wb['ПЭН_ППР сокращ']
+for row in sh.iter_rows():
+    key = row[0].value
 
-    i = 4 if keep3row else 0
+    for dic in dicts:
 
-    for row in sourceSheet.rows:
-        i += 1
-        
-        if i < 4:
-            continue
-        newSheet.append(row)
-        row_index += 1
+        if key in dic.keys():
+            
+            for index in PEN_INDEX_II:
+                row[index - 1].value = dic[key][index - 1]
 
-    keep3row = False
+        dic.clear()
 
-theOne.save(theOneFile)
-# ппр 38-41 50-55 91-98 AL-AM-AN-AO AX-AY-AZ-BA-BB-DC CM-CN-CO-CP-CQ-CR-CS-CT
-# пэн 35-37 42-49 74-90
+    newSheet.append([cell.value for cell in row])
+
+print('clear')
+dicts = None
+
+wb.close()
+wb = None
+
+print('save pen')
+theOne.save(PEN_OUT)
+theOne.close()
 
